beffc2658ae98bb5fe553eb6304aef9ea1d68dc5
Paul Lorenty <paullorenty@gmail.com>
2026-02-27 17:35:32 -0600

    feat(ListaFilas): Lista doblemente enlazada de filas
    Se agregó la clase nodo y lista doblemente enlazada para las filas de pacientes (trabaja en conjunto con las listas de celdas para simular la matriz o rejillas)

---
+++ Modelos/ListaDobleFilas.cs
@@ -0,0 +1,49 @@
+namespace IPC2_Proyecto1_202400173.Modelos
+{
+    public class ListaDobleFilas
+    {
+        private NodoFila? _cabeza;
+        private NodoFila? _cola;
+        private int _totalFilas;
+
+        public NodoFila? Cabeza => _cabeza;
+        public int TotalFilas => _totalFilas;
+
+        public ListaDobleFilas()
+        {
+            _cabeza = null;
+            _cola = null;
+            _totalFilas = 0;
+        }
+
+        // Llenado secuencial
+        public void InsertarFila(int numero)
+        {
+            NodoFila nuevo = new NodoFila(numero);
+            if (_cabeza == null)
+            {
+                _cabeza = nuevo;
+                _cola = nuevo;
+            }
+            else
+            {
+                nuevo.Anterior = _cola;
+                if (_cola != null) _cola.Siguiente = nuevo;
+                _cola = nuevo;
+            }
+            _totalFilas++;
+        }
+
+        // Buscar fila
+        public NodoFila? BuscarFila(int numero)
+        {
+            NodoFila? actual = _cabeza;
+            while (actual != null)
+            {
+                if (actual.NumeroFila == numero) return actual;
+                actual = actual.Siguiente;
+            }
+            return null;
+        }
+    }
+}
